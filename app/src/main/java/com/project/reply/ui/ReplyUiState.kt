ec8f468ac480d03affe31bd0da97560eab5fdccb
MiguelGrilo <miguelgrilo82@gmail.com>
2026-05-17 12:58:05 +0100

    ReplyApp Implementation

---
+++ app/src/main/java/com/project/reply/ui/ReplyUiState.kt
@@ -0,0 +1,14 @@
+package com.project.reply.ui
+
+import com.project.reply.data.Email
+import com.project.reply.data.MailboxType
+import com.project.reply.data.local.LocalEmailsDataProvider
+
+data class ReplyUiState(
+    val mailboxes: Map<MailboxType, List<Email>> = emptyMap(),
+    val currentMailbox: MailboxType = MailboxType.Inbox,
+    val currentSelectedEmail: Email = LocalEmailsDataProvider.defaultEmail,
+    val isShowingHomepage: Boolean = true
+) {
+    val currentMailboxEmails: List<Email> by lazy { mailboxes[currentMailbox]!! }
+}
---
+++ app/src/main/java/com/project/reply/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,8 @@
+package com.project.reply.ui.utils
+
+/**
+ * Different type of navigation supported by app depending on size and state.
+ */
+enum class ReplyNavigationType {
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
